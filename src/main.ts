59962b52c9eaecb287fdbbb60e636edc8e723fcc
mario robayo <marioferyhwh@gmail.com>
2023-11-02 20:47:21 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Api training')
+    .setDescription('The training API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
