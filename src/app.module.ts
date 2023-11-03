0031f574706ac1834604456ebec4982a995ff3f2
mario robayo <marioferyhwh@gmail.com>
2023-11-02 19:03:41 -0500

    create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
