5778abbfc18cbb5bcce41dab0e665fa155b4a4dc
mario robayo <marioferyhwh@gmail.com>
2023-11-04 14:21:41 -0500

    add  documentation

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -9,7 +9,15 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -42,28 +50,44 @@ export class UsersController {
   @Roles(Role.SuperAdmin, Role.Admin)
   @ApiOperation({
     summary: 'create user',
+    description: `require (${Role.SuperAdmin}) o (${Role.Admin}).`,
   })
+  @ApiBody({ type: CreateUserDto })
+  @ApiCreatedResponse({
+    description: 'create user successful',
+    type: CreateUserResponseDto,
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   postUser(@Body() user: CreateUserDto): Promise<CreateUserResponseDto> {
     return this.userService.createUser(user);
   }
 
   @Get(GET_USER)
   @Roles(Role.SuperAdmin, Role.Admin)
-  @ApiOperation({ summary: 'list Users' })
+  @ApiOperation({
+    summary: 'list Users',
+    description: `require (${Role.SuperAdmin}) o (${Role.Admin}).`,
+  })
   getUsers(@Query() params: QueryGetUsersDto) {
     return this.userService.getAllUsersByQuery(params);
   }
 
   @Get(GET_USER_BY_ID)
   @Roles(Role.SuperAdmin, Role.Admin, Role.User)
-  @ApiOperation({ summary: 'get user by id' })
+  @ApiOperation({
+    summary: 'get user by id',
+  })
+  @ApiParam({ name: 'userId', type: 'string', description: 'ID of User' })
   getUser(@Param('userId', MongoIdPipe) userId: string) {
     return this.userService.getUserById(userId);
   }
 
   @Put(PUT_USER_BY_ID)
   @Roles(Role.SuperAdmin, Role.Admin, Role.User)
-  @ApiOperation({ summary: 'update user by id' })
+  @ApiOperation({
+    summary: 'update user by id',
+  })
+  @ApiParam({ name: 'userId', type: 'string', description: 'ID of User' })
   putUser(
     @Param('userId', MongoIdPipe) userId: string,
     @Body() user: UpdateUserDto,
@@ -73,7 +97,11 @@ export class UsersController {
 
   @Delete(DELETE_USER_BY_ID)
   @Roles(Role.SuperAdmin, Role.Admin)
-  @ApiOperation({ summary: 'remove user by id' })
+  @ApiOperation({
+    summary: 'remove user by id',
+    description: `require (${Role.SuperAdmin}) o (${Role.Admin}).`,
+  })
+  @ApiParam({ name: 'userId', type: 'string', description: 'ID of User' })
   deleteUser(@Param('userId', MongoIdPipe) userId: string) {
     return this.userService.deleteUserById(userId);
   }
